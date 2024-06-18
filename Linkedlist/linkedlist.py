da9f1bc052e12c39a5592c59ffe06ec67bc93d9f
srejus <srejusjayakumar@gmail.com>
2024-06-18 20:48:08 +0530

    linkedlist base added

---
+++ Linkedlist/linkedlist.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+    
+    def append(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+    
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        
+        current_node.next = new_node
+        return
+    
+    def prepend(self,data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+    
+    
+        
+
+    
