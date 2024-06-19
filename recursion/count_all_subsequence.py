3fc299d9ca759710687fc16899c84df6c5b6e104
srejus <srejusjayakumar@gmail.com>
2024-06-19 12:21:52 +0530

    count subseq code changes

--- recursion/count_all_subsequence.py
+++ recursion/count_all_subsequence.py
@@ -1,14 +1,12 @@
 arr = [4,1,3]
 n = len(arr)
 
-def func(ind=0,res=[]):
+def func(ind=0):
     if ind >= n:
         return 1
 
-    res.append(arr[ind])
-    l = func(ind+1,res)
-    res.pop()
-    r = func(ind+1,res)
+    l = func(ind+1)
+    r = func(ind+1)
     return l+r
 
 x = func()
