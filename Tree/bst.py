082cec6cb1b3f480a94c1b318edf213feed99929
srejus <srejusjayakumar@gmail.com>
2024-06-19 19:15:57 +0530

    bst changes

--- Tree/bst.py
+++ Tree/bst.py
@@ -3,26 +3,60 @@ class TreeNode:
         self.data = data
         self.left = None
         self.right = None
+        
 
-    
-class BST:
+class BinarySearchTree:
     def __init__(self,data):
         self.root = TreeNode(data)
     
     def add_child(self,data,node=None):
         if not node:
             node = self.root
-        
-        if node.data == data:
+        node_data = node.data
+        if data == node_data:
             return
         
-        if data < node.data:
-            if node.left:
-                self.add_child(data,node.left)
+        if data < node_data:
+            if node.left is None:
+                new_node = TreeNode(data)
+                node.left = new_node
             else:
-                node.left = TreeNode(data)
+                self.add_child(data,self.left)
         else:
-            if node.right:
-                self.add_child(data,node.right)
+            if node.right is None:
+                new_node = TreeNode(data)
+                node.right = new_node
             else:
-                node.right = TreeNode(data)
+                self.add_child(data,self.right)
+                
+                
+                
+    def get_data(self,res=[],node=None):
+        if node is None:
+            node = self.root
+        
+        if node:
+            res.append(node.data)
+            if node.left:
+                self.get_data(res,node.left)
+            if node.right:
+                self.get_data(res,node.right)
+            
+        return res
+    
+    
+
+
+x = BinarySearchTree(10)
+x.add_child(5)
+x.add_child(15)
+data = x.get_data()
+print(data)
+        
+        
+        
+        
+        
+        
+        
+        
