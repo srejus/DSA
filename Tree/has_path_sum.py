c5dd4bfa92035e92af731dff6fcd3c19e5d7d51f
srejus <srejusjayakumar@gmail.com>
2024-06-20 12:44:50 +0530

    Qn added

---
+++ Tree/has_path_sum.py
@@ -0,0 +1,14 @@
+'''Problem 7: Path Sum
+
+Explanation: Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.'''
+
+
+def has_path_sum(node,sm):
+    if not node:
+        return False
+    
+    if not node.left and not node.right:
+        return sm == node.data
+    
+    return has_path_sum(node.left,sm-node.data) or has_path_sum(node.right,sm-node.data)
+
