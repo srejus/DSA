0a79207bc20b717068ddc7967a551eb2877700bd
srejus <srejusjayakumar@gmail.com>
2024-06-26 10:27:50 +0530

    qn updated

--- Tree/delete_tree_node.py
+++ Tree/delete_tree_node.py
@@ -1,21 +1,25 @@
+'''
+Delete node from BST
+'''
+
 def delete_node(node,key):
-        if not node:
-            return node
-        
-        if key < node.data:
-            node.left = delete_node(node.left,key)
-        elif key > node.data:
-            node.right = delete_node(node.right,key)
-        else:
-            if not node.left:
-                return node.right
-            if not node.right:
-                return node.left
-            
-            curr = node
-            while curr.left:
-                curr.left
-            
-            node.data = curr.data
-            node.right = delete_node(node.right,node.data)
+    if not node:
         return node
+    
+    if key < node.data:
+        node.left = delete_node(node.left,key)
+    elif key > node.data:
+        node.right = delete_node(node.right,key)
+    else:
+        if not node.left:
+            return node.right
+        if not node.right:
+            return node.left
+        
+        curr = node
+        while curr.left:
+            curr.left
+        
+        node.data = curr.data
+        node.right = delete_node(node.right,node.data)
+    return node
